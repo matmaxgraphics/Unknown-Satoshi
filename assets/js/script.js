91834d0aed14682c40028bedab91d5ad4db95cc3
Mateen <gbadamosimateen@gmail.com>
2021-10-21 05:25:32 +0100

    started coding the admin panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,22 @@ function classToggle() {
 
 
 
+    /*$(document).ready(function(){
+
+	(function($) {
+
+		$('#header__icon').click(function(e){
+			e.preventDefault();
+			$('body').toggleClass('with--sidebar');
+		});
+    
+    $('#site-cache').click(function(e){
+      $('body').removeClass('with--sidebar');
+    });
+
+	})(jQuery);
+
+});
+*/
+
+
