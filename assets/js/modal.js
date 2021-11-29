e1114476c1af908bcd5ca07c735fb0d474034743
Mateen <gbadamosimateen@gmail.com>
2021-11-29 17:25:56 +0100

    added profile page for users

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -8,7 +8,24 @@ window.onclick = function(event) {
     }
 }
 
-
+//Makes the input field of the wallet address readonly
 document.getElementById('wallet-address').readOnly = true;
 document.getElementById('wallet-address-1').readOnly = true;
 document.getElementById('wallet-address-2').readOnly = true;
+
+
+//Code for controlling the click to copy function
+function copyClick() {
+    const copyText = document.getElementById("wallet-address");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+}
+  
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
