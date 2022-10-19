50baad461287bc823eef7a9664a5d307f8be7837
Mateen.io <gbadamosimateen@gmail.com>
2022-10-19 11:16:28 +0100

    added popup window to shae button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,37 @@
-
 //Navbar Toggling
 function classToggle() {
-    const navs = document.querySelectorAll('.navbar__Items')
-    
-    navs.forEach(nav => nav.classList.toggle('navbar__ToggleShow'));
-  }
-  
-  document.querySelector('.navbar__Link-toggle')
-    .addEventListener('click', classToggle);
-
-navs.forEach(nav=>{
-  nav.addEventListener("click", function () {
-    if (nav.classList.contains("show")) {
-      nav.classList.remove("show")
-    }
-  })
-})
-
-//share button function
-function actionToggle(){
-  var action = document.querySelector('.action');
-  action.classList.toggle('active')
+  const navs = document.querySelectorAll(".navbar__Items");
+  navs.forEach((nav) => nav.classList.toggle("navbar__ToggleShow"));
 }
 
+document
+  .querySelector(".navbar__Link-toggle")
+  .addEventListener("click", classToggle);
 
 
-    
+/*======share button function*========*/
+// function actionToggle() {
+//   const action = document.querySelector(".action");
+//   action.classList.toggle("active");
+// }
+
+/*======like button function*========*/
+const likeBtn = document.querySelector(".like-btn");
+const likeIcon = document.querySelector(".ri-heart-line");
+// const likeNumber = document.querySelector('.action-number__like');
+likeBtn.addEventListener("click", function () {
+  likeIcon.classList.toggle("ri-heart-line");
+  likeIcon.classList.add("ri-heart-fill");
+});
+
+/*======share button function========*/
+const shareBtn = document.querySelector(".share-btn");
+const socialLinks = document.querySelector('.social-sharing');
+
+socialLinks.classList.add('hidden');
 
 
+shareBtn.addEventListener("click", function () {
+  // console.log("button clicked");
+  socialLinks.classList.toggle("hidden");
+});
